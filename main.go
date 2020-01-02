d82b90edfa9e413e5223b828bdbacd1da0af0d84
John Kennedy Bicbic <jkbicbic@gmail.com>
2020-01-02 12:35:03 +0800

    change name from go-paginator to gopager

--- main.go
+++ main.go
@@ -3,21 +3,17 @@ package main
 import (
 	"log"
 
-	"github.com/jkbicbic/go-paginator/paginator"
+	gopager "github.com/jkbicbic/gopager/gopager"
 )
 
 func main() {
 
-	// total rows of your query
-	totalRows := 40
-	// the current page
-	currentPage := 5
-	// the size of your paginator that you will show
-	// e.g. [4 5 6] for size 3 [4 5 6 7 8] for size 5
-	paginatorLimit := 3
+	totalRows := 40     // total rows of your query
+	currentPage := 5    // the current page
+	paginatorLimit := 3 // the size of your paginator e.g. [4 5 6] for size 3 [4 5 6 7 8] for size 5
 
 	// builds a new pagination
-	p := paginator.New(totalRows, currentPage, paginatorLimit)
+	p := gopager.New(totalRows, currentPage, paginatorLimit)
 	p.Paginate()
 
 	// prints the Pagination struct
