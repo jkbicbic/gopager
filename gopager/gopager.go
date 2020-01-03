822fdfcc63d6b7a7c211f18b7f596d121819be08
John Kennedy Bicbic <jkbicbic@gmail.com>
2020-01-03 11:33:43 +0800

    optimized gopager.go

--- gopager/gopager.go
+++ gopager/gopager.go
@@ -11,13 +11,14 @@ type Pagination struct {
 	Next                    uint
 	Pages                   []uint
 	CurrentPage             uint
+	PagerStart              uint
 	totalPages, pagerLength uint
 }
 
 // New builds a new pagination
-func New(totalRows, currentPage, pagerLength uint) *Pagination {
+func New(totalPages, currentPage, pagerLength uint) *Pagination {
 	return &Pagination{
-		totalPages:  totalRows,
+		totalPages:  totalPages,
 		CurrentPage: currentPage,
 		pagerLength: pagerLength,
 	}
@@ -26,7 +27,6 @@ func New(totalRows, currentPage, pagerLength uint) *Pagination {
 // Paginate accepts the totalRows, currentPage, and paginatorLimit to create a paginator for a page
 func (p *Pagination) Paginate() {
 	// new AtPagination struct
-	var pageCalc uint
 
 	p.FirstPage = 1
 	p.LastPage = p.totalPages
@@ -34,11 +34,6 @@ func (p *Pagination) Paginate() {
 	// check if current page overflows totalpage
 	if p.CurrentPage > p.totalPages {
 		p.CurrentPage = p.totalPages
-	}
-
-	// check if current page underflows totalpage
-	if p.CurrentPage < 1 {
-		p.CurrentPage = 1
 	}
 
 	// check if has prev
@@ -54,41 +49,33 @@ func (p *Pagination) Paginate() {
 	// for next page add 1
 	// to currentPage
 	p.Next = p.CurrentPage + 1
-	if p.Next > p.totalPages {
+	if !p.HasNext {
 		p.Next = p.totalPages
 	}
 
 	// for previous page subtract 1
 	// to currentPage
 	p.Prev = p.CurrentPage - 1
-	if p.Prev <= 0 {
+	if !p.HasPrev {
 		p.Prev = 1
 	}
 
 	// check currentPage and generate pages
 	// with different calculations
-	if p.CurrentPage == 1 {
-		pageCalc = p.CurrentPage
-	} else {
-		pageCalc = p.CurrentPage - (p.pagerLength / 2)
+	p.PagerStart = 1
+	if p.CurrentPage != 1 {
+		p.PagerStart = p.CurrentPage - (p.pagerLength / 2)
 	}
 
 	// check to make sure
 	// it will not overflow
 	lastGroup := (p.totalPages - (p.pagerLength - 1))
-	if pageCalc >= lastGroup && pageCalc != 1 {
-		pageCalc = lastGroup
-	}
-
-	// check to make sure
-	// pageCalc will not be less
-	// than zero
-	if pageCalc <= 0 {
-		pageCalc = 1
+	if p.PagerStart >= lastGroup && p.PagerStart != 1 {
+		p.PagerStart = lastGroup
 	}
 
 	// build pages
-	for i := pageCalc; i <= (pageCalc + (p.pagerLength - 1)); i++ {
+	for i := p.PagerStart; i <= (p.PagerStart + (p.pagerLength - 1)); i++ {
 		p.Pages = append(p.Pages, i)
 	}
 
